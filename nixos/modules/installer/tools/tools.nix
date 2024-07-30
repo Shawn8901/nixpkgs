46d1b454f0f6c33ff9edbe2fffaad4b7d47ade35
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-07-30 16:46:51 +0200

    treewide: resolve xkb options renames

--- nixos/modules/installer/tools/tools.nix
+++ nixos/modules/installer/tools/tools.nix
@@ -83,9 +83,9 @@ let
     $xserverConfig
 
     $desktopConfiguration
-      # Configure keymap in X11
-      # services.xserver.xkb.layout = "us";
-      # services.xserver.xkb.options = "eurosign:e,caps:escape";
+      # Configure keymap in X11 and Wayland
+      # environment.xkb.layout = "us";
+      # environment.xkb.options = "eurosign:e,caps:escape";
 
       # Enable CUPS to print documents.
       # services.printing.enable = true;
