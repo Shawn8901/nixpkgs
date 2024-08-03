8a9c590e29d20fc4fbc077722c30c8f579474323
Shawn8901 <shawn8901@googlemail.com>
2024-08-03 21:10:11 +0200

    vmagent: set meta.position so that nixpkgs-update uses its actual derivation definition

--- pkgs/by-name/vm/vmagent/package.nix
+++ pkgs/by-name/vm/vmagent/package.nix
@@ -3,14 +3,21 @@
 # This package is build out of the victoriametrics package.
 # so no separat update prs are needed for vmagent
 # nixpkgs-update: no auto update
-lib.addMetaAttrs { mainProgram = "vmagent"; } (
-  victoriametrics.override {
-    withServer = false;
-    withVictoriaLogs = false;
-    withVmAlert = false;
-    withVmAuth = false;
-    withBackupTools = false;
-    withVmctl = false;
-    withVmAgent = true;
+lib.addMetaAttrs
+  {
+    mainProgram = "vmagent";
+    # Add position, so that nixpkgs-update uses this for update parsing
+    # (including ignore definitions) and not victoriametrics derivation
+    position = toString ./package.nix + ":1";
   }
-)
+  (
+    victoriametrics.override {
+      withServer = false;
+      withVictoriaLogs = false;
+      withVmAlert = false;
+      withVmAuth = false;
+      withBackupTools = false;
+      withVmctl = false;
+      withVmAgent = true;
+    }
+  )
